4a8ce581746866b90ae742ca17918389a8e58cd2
taehee <wonderfulhuman@naver.com>
2021-09-22 21:51:18 +0900

    Add: Test를 docker-compose로 자동화

--- docker-react-app/src/App.js
+++ docker-react-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactdsafasddasfasdfsss
+          Learn Reactdsafasddasfasdfssssadfsadsdsdsdsd
         </a>
       </header>
     </div>
